9f01024a9b71aa00532b7452d113e2a9949fb43a
fongsd <72883620+fongsd@users.noreply.github.com>
2022-05-22 12:27:22 +0200

    Add files via upload

--- 134.c
+++ 134.c
@@ -5,11 +5,11 @@ int main(){
 
     double masa = 5.98 * pow(10,24);
     // printf("%f\n", masa);
-    float r = 6371;
+    float r = 6371000;
     float g = 0;
     
 
-    for (float h = 200; h <= 800; h+=50){
+    for (float h = 200000; h <= 800000; h+=50000){
 
         g=-(6.672 * pow(10, -11) * 5.98 * pow(10,24))/((r+h)*(r+h));
         printf("%f\n", g);
